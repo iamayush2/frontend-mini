105ed0ac2f7b31ba4ed02461e603fc0260420a17
Ayush Dulhani <99423216+iamayush2@users.noreply.github.com>
2023-12-04 18:03:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+let select = document.querySelector(".select");
+let farmer = document.querySelector(".farmer");
+let cust = document.querySelector(".coustmer");
+let login = document.querySelector(".login-btn");
+let back = document.querySelector(".back-btn");
+let admin = document.querySelector(".administor");
+select.addEventListener("click", function (e) {
+  console.log("Hello");
+  select.style.display = "none";
+  admin.style.display = "flex";
+  farmer.style.display = "flex";
+  cust.style.display = "flex";
+  back.style.display = "flex";
+});
+
+// dynamic container
+
+let mainDynamicCont = document.querySelector(".main-dynamic-cont");
+let optName = document.querySelector(".opt-name");
+
+farmer.onclick = () => {
+  mainDynamicCont.style.display = "flex";
+  select.style.display = "none";
+  farmer.style.display = "none";
+  admin.style.display = "none";
+  cust.style.display = "none";
+  login.style.display = "flex";
+  optName.textContent = "Farmer";
+};
+
+cust.onclick = () => {
+  mainDynamicCont.style.display = "flex";
+  select.style.display = "none";
+  admin.style.display = "none";
+  farmer.style.display = "none";
+  login.style.display = "flex";
+  cust.style.display = "none";
+  optName.textContent = "Customer";
+};
+admin.onclick = () => {
+  mainDynamicCont.style.display = "flex";
+  select.style.display = "none";
+  admin.style.display = "none";
+  farmer.style.display = "none";
+  login.style.display = "flex";
+  cust.style.display = "none";
+  optName.textContent = "Administer";
+};
+
+back.onclick = () => {
+  mainDynamicCont.style.display = "none";
+
+  select.style.display = "flex";
+  farmer.style.display = "none";
+  login.style.display = "none";
+  cust.style.display = "none";
+  admin.style.display = "none";
+  back.style.display = "none";
+};
